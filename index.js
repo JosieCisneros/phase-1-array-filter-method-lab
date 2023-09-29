6d285bc634c6f37ec50dbb9427acc62689cddc3c
Josie Cisneros <josiecisneros1224@gmail.com>
2023-09-29 16:40:48 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+function findMatching (array, string) {
+    return array.filter (function(str) {
+        return str.toLowerCase() === string.toLowerCase()
+    }) 
+}
+
+findMatching(array, string) 
+
+function fuzzyMatch (array, string) {
+    return array.filter((el) => el.toLowerCase().substring (0, string.length) === string.toLowerCase())
+}
+
+fuzzyMatch(array, string)
+
+function matchName (array, string) {
+    return array.filter (function(obj) {
+        return obj.name === string
+    })
+}
+
+matchName (array, string)
